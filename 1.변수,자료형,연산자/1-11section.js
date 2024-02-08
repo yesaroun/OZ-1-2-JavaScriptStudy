6f7b93c5461c887de91a8b302e336dc1e41730f0
yesaroun <akor1@naver.com>
2024-02-09 02:01:20 +0900

    오타 수정

--- 1.변수,자료형,연산자/1-11section.js
+++ 1.변수,자료형,연산자/1-11section.js
@@ -9,7 +9,7 @@ let b = false;
 console.log("a && b : ", (a && b)); // false
 // OR
 console.log("a || b : ", (a || b)); // true
-// Not
+// NOT
 console.log("!a : ", !a); // false
 console.log("!b : ", !b); // true
 
@@ -20,7 +20,7 @@ console.log("----------------------");
 // 조건식에서 false로 간주되는 값들을 falsy 값이라고 한다.
 // false
 // 0
-/// "" (빈 문자열)
+// "" (빈 문자열)
 // null : 값이 없음을 나타내는 값
 // undefined : 값이 할당되지 않음을 나타내는 값
 // NaN : 숫자가 아님을 나타내는 값
@@ -34,12 +34,12 @@ console.log(Boolean(0));          // false
 console.log(Boolean(""));         // false
 console.log(Boolean(null));       // false
 console.log(Boolean(undefined));  // false
-console.log(Boolean(NaN));              // false
+console.log(Boolean(NaN));        // false
 
 // truthy 값들
-console.log(Boolean(true));         // true
-console.log(Boolean(1));            // true
-console.log(Boolean("hello"));      // true
-console.log(Boolean([]));           // true
-console.log(Boolean({}));           // true
-console.log(Boolean(function(){})); // true
+console.log(Boolean(true));          // true
+console.log(Boolean(1));             // true
+console.log(Boolean("hello"));       // true
+console.log(Boolean([]));            // true
+console.log(Boolean({}));            // true
+console.log(Boolean(function(){}));  // true
